ca7c62daee0bf6fc105faee6cba1a00a013790c6
shivam9pal <shivampaul9794@gmail..com>
2026-01-03 17:20:42 +0530

    [Shivam Pal]Refactored. student score

--- oops-practice/scenario-based/StudentScores.java
+++ oops-practice/scenario-based/StudentScores.java
@@ -1,63 +1,104 @@
 package week02.scenario;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class StudentScores {
-	public static void analyzeTemperature(float[][] temp) {
-
-	    int hottestDay = 0;
-	    int coldestDay = 0;
-	    float maxTemp = temp[0][0];
-	    float minTemp = temp[0][0];
-
-	    for (int i = 0; i < temp.length; i++) {
-	        float sum = 0;
-	        for (int j = 0; j < temp[i].length; j++) {
-
-	            sum += temp[i][j];
-
-	            if (temp[i][j] > maxTemp) {
-	                maxTemp = temp[i][j];
-	                hottestDay = i;
-	            }
-	            if (temp[i][j] < minTemp) {
-	                minTemp = temp[i][j];
-	                coldestDay = i;
-	            }
-	        }
-	        float average = sum / temp[i].length;
-	        System.out.println("Average temperature of day " + (i + 1)
-	                + " = " + average);
-	    }
-	    System.out.println("Hottest day: Day " + (hottestDay + 1));
-	    System.out.println("Coldest day: Day " + (coldestDay + 1));
+	
+	static int [] storeScore(int n) {
+		String st = inputScore(n, 1);
+		System.out.println("Scores: "+st);
+		
+		return Arrays.stream(st.split(" ")).mapToInt(Integer :: parseInt).toArray();
+	}
+	
+	static String inputScore(int n, int m) {
+		Scanner sc = new Scanner(System.in);
+		try {
+			
+			if(n==1) {
+				System.out.print("Enter marks of student "+m+": ");
+				int score = sc.nextInt();
+				sc.nextLine();
+				
+				if(score>=0 && score<=100) {
+					sc.close();
+					return score+"";
+					
+				}else {
+					System.out.println("Enter score between 0 and 100.");
+					return inputScore(n,m)+"";
+				}
+				
+				
+			}
+			System.out.print("Enter marks of student "+m+": ");
+			int score = sc.nextInt();
+			sc.nextLine();
+			
+			if(score>=0 && score<=100) {
+				return score+" "+inputScore(n-1, m+1);
+			}else {
+				System.out.println("Enter score between 0 and 100.");
+				return inputScore(n,m)+"";
+			}
+			
+		}
+		catch(Exception e) {
+			sc.nextLine();
+			System.out.println("Invalid input...");
+			return inputScore(n,m)+"";
+		}
+	
+		
+	}
+	
+	static float avg(int[] arr) {
+		int sum = 0;
+		for(int x:arr) {
+			sum+=x;
+		}
+		return Math.round((sum/arr.length)*100.0f)/100.0f;
+	}
+	
+	static void aboveAvg(int[] arr) {
+		float avg = avg(arr);
+		for(int x:arr) {
+			if (x>avg) {
+				System.out.print(x+" ");
+			}
+		}
+		System.out.println();
+	}
+	
+	static void maxAndMin(int[] arr) {
+		int max=arr[0], min=arr[0];
+		for(int x:arr) {
+			if(max<x) {
+				max=x;
+			}
+			if(min>x) {
+				min=x;
+			}
+		}
+		System.out.println("Highest Marks: "+max);
+		System.out.println("Lowest Marks: "+min);
 	}
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of students: ");
-        int n = sc.nextInt();
-        int[] scores = new int[n];
-        int sum = 0;
+    	Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of Students : ");
+		int n = sc.nextInt();
+		int[] stArray = storeScore(n);
+		
+		System.out.println("Average Score: "+avg(stArray));
+		System.out.print("Scores above average score: ");
+		aboveAvg(stArray);
+		maxAndMin(stArray);
+		
 
-        for (int i = 0; i < n; i++) {
-            System.out.print("Enter score of student " + (i + 1) + ": ");
-            if (!sc.hasNextInt()) {
-                System.out.println("Invalid input! Enter numbers only.");
-                return;
-            }
-            int score = sc.nextInt();
-            if (score < 0) {
-                System.out.println("Score cannot be negative.");
-                return;
-            }
-            scores[i] = score;
-            sum += score;
-        }
-
-        double average = (double) sum / n;
-        analyzeTemperature();
-        sc.close();
+		sc.close();
+		}
     }
-}
+
 
